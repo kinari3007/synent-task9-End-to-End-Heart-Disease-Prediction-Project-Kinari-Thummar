d529a40dbf4d075c535c1e0b1c7ad50e028c437e
Kinu <kinarithummar3007@gmail.com>
2026-06-02 16:51:34 +0530

    Chore : initialize project structure with setup, requirements and gitignore

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='heart_disease_prediction',
+    version='0.0.1',
+    author='Kinari Thummar',
+    packages=find_packages(),
+    install_requires=[]
+)
